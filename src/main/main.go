fa45ceced61a1d3cd98f6c10034c605f91f0d7a7
Alex Pagan <alexrpagan@gmail.com>
2013-05-23 17:29:19 -0400

    Add basic gui, tile re-arrangement algo

--- src/main/main.go
+++ src/main/main.go
@@ -1,76 +1,122 @@
 package main
 
 import (
-    "fmt"
-    "pixl"
-    "time"
-    "math/rand"
-    "flag"
-    "os"
-    "bufio"
-    "image/color"
+	"fmt"
+	"pixl"
+	"time"
+	"math"
+	"math/rand"
+	"flag"
+	"os"
+	"bufio"
+	"image/color"
+
+	"x-go-binding/ui"
+	"x-go-binding/ui/x11"
+
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
 )
 
-var input     = flag.String("i", "", "input file")
-var output    = flag.String("o", "out.png", "output file")
+var input = flag.String("i", "", "input file")
+var output = flag.String("o", "out.png", "output file")
 var shuffle   = flag.Bool("s", false, "shuffle the pixels?")
+var sortrows  = flag.Bool("sr", false, "sort the rows?")
 var blocksize = flag.Int("b", 10, "blocksize")
+var iters = flag.Int("iters", 0, "number of iterations of clustering algorithm to perform")
 
 func random (x, y int, p *pixl.Pixl) color.Color {
-    subImg := p.Image.SubImage(p.GetBlock(x, y))
-    bounds := subImg.Bounds()
-    offsetX := rand.Int() % p.BlockSize
-    offsetY := rand.Int() % p.BlockSize
-    return p.Image.At(bounds.Min.X + offsetX, bounds.Min.Y + offsetY)
+	subImg := p.Image.SubImage(p.GetBlock(x, y))
+	bounds := subImg.Bounds()
+	offsetX := rand.Int() % p.BlockSize
+	offsetY := rand.Int() % p.BlockSize
+	return p.Image.At(bounds.Min.X + offsetX, bounds.Min.Y + offsetY)
 }
 
 func unbiased (p *pixl.Pixl, x1, y1, x2, y2 int) bool {
-    return true
+	return true
 }
 
-func bluey (p *pixl.Pixl, x1, y1, x2, y2 int) bool {
-    _, _, b1, _ := random(x1, y1, p).RGBA()
-    return b1 < 30000
+func euclid(c1 color.Color, c2 color.Color) float64 {
+	r1, g1, b1, _ := c1.RGBA()
+	r2, g2, b2, _ := c2.RGBA()
+
+	// convert to YUI
+	y1, cb1, cr1 := color.RGBToYCbCr(uint8(r1), uint8(g1), uint8(b1))
+	y2, cb2, cr2 := color.RGBToYCbCr(uint8(r2), uint8(g2), uint8(b2))
+
+	score := math.Sqrt(math.Pow(float64(y1-y2),2) + math.Pow(float64(cb1-cb2),2) + math.Pow(float64(cr1-cr2),2))
+	return score
 }
 
 func main () {
 
-    rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 
-    flag.Parse()
+	flag.Parse()
 
-    if *input != "" {
+	if *input == "" {
+		fmt.Println("No input image!")
+	}
 
-        pix := new(pixl.Pixl)
+	pix := new(pixl.Pixl)
 
-        inf, err := os.Open(*input)
-        defer inf.Close()
-        if err == nil {
-            reader := bufio.NewReader(inf)
-            if pix.Decode(reader) != nil {
-                os.Exit(1)
-            }
-        }
+	inf, err := os.Open(*input)
+	defer inf.Close()
+	if err == nil {
+		reader := bufio.NewReader(inf)
+		if pix.Decode(reader) != nil {
+			fmt.Println("file open failed")
+			os.Exit(1)
+		}
+	}
 
-        pix.Pixelate(*blocksize, random)
+	// resize the window to fit the picture
+	bounds := pix.Image.Bounds()
 
-        if *shuffle {
-            pix.Shuffle(unbiased)
-        }
+	w, err := x11.NewWindow(bounds.Dx(), bounds.Dy())
+	if err != nil {
+			fmt.Println(err)
+			return
+	}
+	pix.Window = w
 
-        pix.SortRows()
+	pix.Pixelate(*blocksize, random)
 
-        outf, err := os.Create(*output)
-        if err == nil {
-            writer := bufio.NewWriter(outf)
-            pix.Encode(writer)
-        } else {
-            fmt.Println(err)
-            os.Exit(1)
-        }
-        outf.Close()
+	if *shuffle {
+		pix.Shuffle(unbiased)
+	}
 
-    }
+	// run the clustering algo iters times
+	if *iters != 0 {
+		for i:=0; i < *iters; i++ {
+			pix.DoStep(5, euclid)
+			fmt.Println(i)
+		}
+	}
+
+	pix.WriteToScreen()
+
+	for e := range w.EventChan() {
+		switch e := e.(type) {
+		case ui.KeyEvent:
+			if e.Key == ' ' { // perform another iteration
+				pix.DoStep(5, euclid)
+				pix.WriteToScreen()
+			} else if e.Key == 's' { // save image
+				outf, err := os.Create(*output)
+				defer outf.Close()
+				if err == nil {
+					writer := bufio.NewWriter(outf)
+					pix.Encode(writer)
+				} else {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+			}
+		}
+	}
 
 }
 
--- src/pixl/pixl.go
+++ src/pixl/pixl.go
@@ -1,184 +1,262 @@
 package pixl
 
 import (
-    "sort"
-    "image"
-    "image/png"
-    "image/color"
-    "image/draw"
-    "io"
-    "math/rand"
-    _ "image/jpeg"
+	"io"
+	// "fmt"
+	"sort"
+	"image"
+	"image/draw"
+	"image/png"
+	"image/color"
+	"math"
+	"math/rand"
+
+	"x-go-binding/ui"
+
+	_ "image/jpeg"
 )
 
 type Pixeler interface {
 
-    Decode(r io.Reader) error
+	Decode(r io.Reader) error
 
-    Encode(w io.Writer) error
+	Encode(w io.Writer) error
 
-    // Down-sample image based on desired resolution and aggregate function
-    Pixelate(nb int, f func (x, y int, p *Pixeler) color.Color) error
+	// Down-sample image based on desired resolution and aggregate function
+	Pixelate(nb int, f func (x, y int, p *Pixeler) color.Color) error
 
-    // swap two tiles
-    Swap(x1, y1, x2, y2 int) error
+	// swap two tiles
+	Swap(x1, y1, x2, y2 int) error
 
-    // Rearrange tiles, using f to bias the shuffle
-    Shuffle(f func(x1, y1, x2, y2 int) bool) error
+	// Rearrange tiles, using f to bias the shuffle
+	Shuffle(f func(x1, y1, x2, y2 int) bool) error
 
-    // Gets the bounding box for a specific block
-    GetBlock(x, y int) image.Rectangle
+	// Gets the bounding box for a specific block
+	GetBlock(x, y int) image.Rectangle
 
-    FillBlock(x, y int, c color.Color)
+	FillBlock(x, y int, c color.Color)
 
 }
 
 type Pixl struct {
-    Image *image.RGBA
-    NumCols int
-    NumRows int
-    BlockSize int
+	Image *image.RGBA
+	NumCols int
+	NumRows int
+	BlockSize int
+	Window ui.Window
 }
 
 func (p *Pixl) Decode(r io.Reader) error {
-    img, _, err := image.Decode(r)
-    if err == nil {
-        newImg := image.NewRGBA(img.Bounds())
-        draw.Draw(newImg, newImg.Bounds(), img, image.ZP, draw.Src)
-        p.Image = newImg
-    }
-    return err
+	img, _, err := image.Decode(r)
+	if err == nil {
+		newImg := image.NewRGBA(img.Bounds())
+		draw.Draw(newImg, newImg.Bounds(), img, image.ZP, draw.Src)
+		p.Image = newImg
+	}
+	return err
 }
 
 func (p *Pixl) Encode(w io.Writer) error {
-    return png.Encode(w, p.Image)
+	return png.Encode(w, p.Image)
 }
 
 func (p *Pixl) Pixelate(nb int, f func (x, y int, p *Pixl) color.Color) error {
 
-    bounds := p.Image.Bounds()
-    width  := bounds.Dx()
-    height := bounds.Dy()
+	bounds := p.Image.Bounds()
+	width  := bounds.Dx()
+	height := bounds.Dy()
 
-    p.NumCols = nb
-    p.BlockSize = width / nb
-    p.NumRows = height / p.BlockSize
+	p.NumCols = nb
+	p.BlockSize = width / nb
+	p.NumRows = height / p.BlockSize
 
-    var x, y int
+	var x, y int
 
-    // columns
-    for x=0; x < p.NumCols; x++ {
-        // rows
-        for y=0; y < p.NumRows; y++ {
-            p.FillBlock(x, y, f(x, y, p))
-        }
-    }
+	// columns
+	for x=0; x < p.NumCols; x++ {
+		// rows
+		for y=0; y < p.NumRows; y++ {
+			p.FillBlock(x, y, f(x, y, p))
+		}
+	}
 
-    p.Crop()
-    return nil
+	p.Crop()
+	return nil
 }
 
 func (p *Pixl) Crop() {
-    subImg := p.Image.SubImage(image.Rect(0,0, p.NumCols * p.BlockSize, p.NumRows * p.BlockSize))
-    newImg := image.NewRGBA(subImg.Bounds())
-    draw.Draw(newImg, newImg.Bounds(), subImg, image.ZP, draw.Src)
-    p.Image = newImg
+	subImg := p.Image.SubImage(image.Rect(0,0, p.NumCols * p.BlockSize, p.NumRows * p.BlockSize))
+	newImg := image.NewRGBA(subImg.Bounds())
+	draw.Draw(newImg, newImg.Bounds(), subImg, image.ZP, draw.Src)
+	p.Image = newImg
 }
 
 func (p *Pixl) ColorAt(x, y int) color.Color {
-    return p.random(x,y)
+	return p.random(x,y)
 }
 
 func (p *Pixl) Swap(x1, y1, x2, y2 int) error {
-    c1 := p.ColorAt(x1, y1)
-    c2 := p.ColorAt(x2, y2)
-    p.FillBlock(x2, y2, c1)
-    p.FillBlock(x1, y1, c2)
-    return nil
+	c1 := p.ColorAt(x1, y1)
+	c2 := p.ColorAt(x2, y2)
+	p.FillBlock(x2, y2, c1)
+	p.FillBlock(x1, y1, c2)
+	return nil
 }
 
 // fisher-yates-ish shuffle
 func (p *Pixl) Shuffle(f func(p *Pixl, x1, y1, x2, y2 int) bool) error {
-    for i:= p.NumCols * p.NumRows - 1; i > 0; i-- {
-        x1, y1 := p.GetXY(i)
-        x2, y2 := p.GetXY(rand.Int() % (i + 1))
-        if f(p, x1, y1, x2, y2) {
-            p.Swap(x1, y1, x2, y2)
-        }
-    }
-    return nil
+	for i:= p.NumCols * p.NumRows - 1; i > 0; i-- {
+		x1, y1 := p.GetXY(i)
+		x2, y2 := p.GetXY(rand.Int() % (i + 1))
+		if f(p, x1, y1, x2, y2) {
+			p.Swap(x1, y1, x2, y2)
+		}
+	}
+	return nil
 }
 
+
+func (p *Pixl) DoStep(frequency int, dist func (color.Color, color.Color) float64) {
+	numCells := p.NumCols * p.NumRows
+
+	// FIXME
+	iters := int(math.Floor(float64(numCells) * float64(frequency)/float64(100)))
+
+	for i:=0; i < iters; i++ {
+		bn := rand.Int() % numCells
+		x, y := p.GetXY(bn)
+
+		var minScore, score float64
+		var minX, minY, newX, newY int
+
+		minScore = 10000000000000000 // TODO: fix this
+		minX = -1
+		minY = -1
+
+		for xDelta := -1; xDelta < 2; xDelta++ {
+			for yDelta:= -1; yDelta < 2; yDelta++ {
+
+				score = 0
+				newX = xDelta + x
+				newY = yDelta + y
+
+				// sanity check
+				if newX >= 0 && newX < p.NumCols && newY >= 0 && newY < p.NumRows {
+
+					// all nearest neighbors
+					for xd2 := -1; xd2 < 2; xd2++ {
+						for yd2 := -1; yd2 < 2; yd2++ {
+
+							// neighboring point to mover
+							newX2 := xd2 + newX
+							newY2 := yd2 + newY
+
+							// neighboring point to moved point
+							newX3 := xd2 + x
+							newY3 := yd2 + y
+
+							if newX2 >= 0 && newX2 < p.NumCols && newY2 >= 0 && newY2 < p.NumRows {
+
+								// dissimilarity of points close to mover in hypothetical position
+								score += dist(p.ColorAt(x,y), p.ColorAt(newX2, newY2))
+
+								// dissimilarity of points closed to moved in hypothetical position
+								score += dist(p.ColorAt(newX, newY), p.ColorAt(newX3, newY3))
+
+							}
+						}
+					}
+
+					if score < minScore {
+						minScore = score
+						minX = newX
+						minY = newY
+					}
+				}
+			}
+		}
+		if minX >= 0 && minY >= 0 {
+			p.Swap(x,y, minX, minY)
+		}
+	}
+}
+
+
 func (p *Pixl) GetXY(bn int) (x int, y int) {
-    x = (bn % p.NumCols)
-    y = (bn / p.NumCols)
-    return x, y
+	x = (bn % p.NumCols)
+	y = (bn / p.NumCols)
+	return x, y
 }
 
 func (p *Pixl) GetBlock(x, y int) image.Rectangle {
-    bs := p.BlockSize
-    return image.Rect(x*bs, y*bs, (x+1)*bs, (y+1)*bs)
+	bs := p.BlockSize
+	return image.Rect(x*bs, y*bs, (x+1)*bs, (y+1)*bs)
 }
 
 func (p *Pixl) FillBlock(x int, y int, c color.Color) {
-    draw.Draw(p.Image, p.GetBlock(x, y), &image.Uniform{c}, image.ZP, draw.Src)
+	draw.Draw(p.Image, p.GetBlock(x, y), &image.Uniform{c}, image.ZP, draw.Src)
 }
 
 func (p *Pixl) SortRows() {
-    for i:=0; i<p.NumRows; i++ {
-        sp := new(SubPixl)
-        sp.Init(p, i*p.NumCols, (i+1)*p.NumCols)
-        sp.Sort()
-    }
+	for i:=0; i<p.NumRows; i++ {
+		sp := new(SubPixl)
+		sp.Init(p, i*p.NumCols, (i+1)*p.NumCols)
+		sp.Sort()
+	}
+}
+
+func (p *Pixl) WriteToScreen() {
+	draw.Draw(p.Window.Screen(), p.Window.Screen().Bounds(), p.Image, image.ZP, draw.Src)
+	p.Window.FlushImage()
 }
 
 func (p *Pixl) random (x, y int) color.Color {
-    subImg  := p.Image.SubImage(p.GetBlock(x, y))
-    bounds  := subImg.Bounds()
-    offsetX := rand.Int() % p.BlockSize
-    offsetY := rand.Int() % p.BlockSize
-    return p.Image.At(bounds.Min.X + offsetX, bounds.Min.Y + offsetY)
+	subImg  := p.Image.SubImage(p.GetBlock(x, y))
+	bounds  := subImg.Bounds()
+	offsetX := rand.Int() % p.BlockSize
+	offsetY := rand.Int() % p.BlockSize
+	return p.Image.At(bounds.Min.X + offsetX, bounds.Min.Y + offsetY)
 }
 
 // represents a consecutive run of pixels
 type SubPixl struct {
-    p *Pixl
-    Start int
-    End int
+	p *Pixl
+	Start int
+	End int
 }
 
 func (sp *SubPixl) Init(p *Pixl, s, e int) {
-    sp.p = p
-    sp.Start = s
-    sp.End = e
+	sp.p = p
+	sp.Start = s
+	sp.End = e
 }
 
 func (sp *SubPixl) Len() int {
-    return (sp.End + 1) - sp.Start
+	return (sp.End + 1) - sp.Start
 }
 
 func (sp *SubPixl) GetXY(i int) (x, y int) {
-    x, y = sp.p.GetXY(sp.Start + i)
-    return x,y
+	x, y = sp.p.GetXY(sp.Start + i)
+	return x,y
 }
 
 func (sp *SubPixl) Color(i int) color.Color {
-    return sp.p.ColorAt(sp.GetXY(i))
+	return sp.p.ColorAt(sp.GetXY(i))
 }
 
 func (sp *SubPixl) Swap(i, j int) {
-    x1, y1 := sp.GetXY(i)
-    x2, y2 := sp.GetXY(j)
-    sp.p.Swap(x1, y1, x2, y2)
+	x1, y1 := sp.GetXY(i)
+	x2, y2 := sp.GetXY(j)
+	sp.p.Swap(x1, y1, x2, y2)
 }
 
 func (sp *SubPixl) Less(i, j int) bool {
-    r1, g1, b1, _ := sp.Color(i).RGBA()
-    r2, g2, b2, _ := sp.Color(j).RGBA()
-    // currently sorts on blueness
-    return ((r1 < r2 || g1 < g2) && false) || b1 < b2 // TODO: less arbitrary criterion
+	r1, g1, b1, _ := sp.Color(i).RGBA()
+	r2, g2, b2, _ := sp.Color(j).RGBA()
+	// currently sorts on blueness
+	return ((r1 < r2 || b1 < b2 && false) || g1 < g2) // TODO: less arbitrary criterion
 }
 
 func (sp *SubPixl) Sort() {
-    sort.Sort(sp)
+	sort.Sort(sp)
 }
